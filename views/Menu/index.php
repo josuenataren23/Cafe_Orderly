c795b58041df9ecdbd259fdbbba5e5fa66388912
josuenataren23 <josue.nataren23@unach.mx>
2025-10-19 03:58:55 -0600

    Avance Interfaz Menu 19/10

--- views/Menu/index.php
+++ views/Menu/index.php
@@ -1,3 +1,35 @@
-<div class="banner">
-    <h1>Menu</h1>
-</div>
+<section class="banner">
+    <div class="conten-text-banner animate-fade-in">
+        <h1>Descubre Nuestro Menú</h1>
+        <p>Deliciosas opciones para todos los gustos</p>
+    </div>
+    <div class="conten-cards-banner animate-fade-in">
+        <div class="card-menu-banner animate-fade-in">
+            <div class="conten-img-card-banner">
+                <img src="./views/assets/img/americano.jpg" alt="Desayunos">
+            </div>
+            <h3>Café y Bebidas</h3>
+            <p>Explora nuestra selección de cafés artesanales, tés y bebidas especiales.</p>
+        </div>
+        <div class="card-menu-banner animate-fade-in">
+            <div class="conten-img-card-banner">
+                <img src="./views/assets/img/chilaquiles.jpg" alt="Desayunos">
+            </div>
+            <h3>Desayunos</h3>
+            <p>Comienza tu día con nuestros deliciosos desayunos caseros y saludables.</p>
+        </div>
+        <div class="card-menu-banner animate-fade-in">
+            <div class="conten-img-card-banner">
+                <img src="./views/assets/img/Cheesecake.jpg" alt="Desayunos">
+            </div>
+            <h3>Postres</h3>
+            <p>Endulza tu día con nuestra variedad de postres frescos y caseros.</p>
+        </div>
+    </div>
+    <a class="btn-vermenu animate-fade-in" href="index.php?controller=home&action=menu#section-menu" class="btn-menu animate-fade-in">Ver Menú</a>
+    <span class="flecha-abajo animate-fade-in"><i class="fa-solid fa-chevron-down"></i></span>
+</section>
+
+<section id="section-menu" class="section-menu">
+    <h1>Aqui va el menú</h1>
+</section>
