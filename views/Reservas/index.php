6ba5ddc8b7c023050c127450f72e35e5ed01d700
josuenataren23 <josue.nataren23@unach.mx>
2025-10-20 00:21:04 -0600

    Landing de reservar

--- views/Reservas/index.php
+++ views/Reservas/index.php
@@ -1,1 +1,8 @@
-<h1>Reservas</h1>
+<section class="banner">
+    <h1>Inicia Sesion Para Reservar</h1>
+    <a href="index.php?controller=reservar&action=login" class="btn-login">Iniciar Sesion</a>
+    <span class="info-login">
+        <p>Mas Informacion</p>
+        <i class="fa-solid fa-chevron-down"></i>
+    </span>
+</section>
